626fac2c9a349568ddf3d967b56c6ebdea1d0fc6
Jesse Dirisu <dirisujesse@gmail.com>
2023-06-09 22:12:01 +0100

    Initial Commit

---
+++ PetShop/PetShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetShopApp.swift
+//  PetShop
+//
+//  Created by Dirisu on 09/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PetShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
